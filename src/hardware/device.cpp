c566931973c85ad11587e21aeb8fcc50e2951395
kjh2159 <kjh2159@dgist.ac.kr>
2025-09-23 21:07:40 +0900

    add: ro.product.model for dvfs

--- src/hardware/device.cpp
+++ src/hardware/device.cpp
@@ -3,7 +3,8 @@
 Device::Device(const std::string& device_name) : device(device_name){
     if (device_name == "S22_Ultra" || device_name == "Fold4" ||  device_name == "Pixel9"){
 	    cluster_indices = {0, 4, 7};
-    } else if (device_name == "S24"){
+    } else if (device_name == "S24" || device_name == "SM-S921N"){
+        // SM-S921N: Galaxy S24
 	    cluster_indices = {0, 4, 7, 9};
     }
 }
