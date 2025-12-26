70683df7daa3282967fc86d80e24fb6407da9af5
JIN <63086170+kjh2159@users.noreply.github.com>
2025-12-26 23:44:40 +0900

    update: ignite-qwen.sh
    Add commands to reset CPU cores 1, 2, and 3.

--- scripts-termux/ignite-qwen.sh
+++ scripts-termux/ignite-qwen.sh
@@ -48,6 +48,11 @@ su -c "echo 0 > /sys/devices/system/cpu/cpu3/online"
 # [pause-unit] = ms
 # [interval-unit] = s
 
+# silver core reset
+su -c "echo 1 > /sys/devices/system/cpu/cpu1/online"
+su -c "echo 1 > /sys/devices/system/cpu/cpu2/online"
+su -c "echo 1 > /sys/devices/system/cpu/cpu3/online"
+
 # turn-on screen
 # turn-off screen
 if [ "$DEV" = "Pixel9" ]; then
