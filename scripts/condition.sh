195381db367510210ebcb0bc0dcba96d63246f7f
kukhyeon <sha5686a@example.com>
2026-02-18 18:10:58 +0900

    Update: Initial Conditon

--- scripts/condition.sh
+++ scripts/condition.sh
@@ -1,22 +1,5 @@
 #!/system/bin/sh
 
-echo "[*] Disabling SELinux Enforcing..."
-setenforce 0
-
-echo "[*] Disabling ZRAM Swap..."
-swapoff /dev/block/zram0
-
-echo "[*] Dropping Caches (Memory Optimization)..."
-sync
-echo 3 > /proc/sys/vm/drop_caches
-
-echo "[*] Stopping Thermal Daemons..."
-stop thermal-engine
-stop vendor.samsung.hardware.thermal-default
-
-echo "[*] Checking Thermal Properties..."
-getprop | grep thermal
-
 echo "[*] Disabling Samsung GOS & Game Tools..."
 pm disable-user com.samsung.android.game.gos
 pm disable-user com.samsung.android.game.gametools
